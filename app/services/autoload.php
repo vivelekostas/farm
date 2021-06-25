33083e10a54f8c84a4318f91799ff7b012721833
KonstantinGrebenyuk <vivelakostas1096@gmail.com>
2021-06-25 13:31:39 +0300

    debug

---
+++ app/services/autoload.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'app\classes\interfaces\AnimalInterface.php';
+require_once 'app\classes\animals\Animal.php';
+require_once 'app\classes\animals\Cow.php';
+require_once 'app\classes\animals\Chicken.php';
+require_once 'app\classes\Farm.php';
+require_once 'app\services\mainService.php';
--- app/services/mainService.php
+++ app/services/mainService.php
@@ -1,43 +1,38 @@
 <?php
 
-use \app\classes\animals\Cow;
-use \app\classes\animals\Chicken;
 use \app\classes\Farm;
 
-function addNewAnimals(array $newAnimals, $farm)
+
+function addNewAnimals(array $newAnimals, Farm $farm)
 {
+
+
     foreach ($newAnimals as $animal => $quantity) {
-        $counter = 1;
-        if ($animal === 'cow') {
-            while ($counter <= $quantity) {
-                $farm->addAnimal(new Cow(uniqid('cow')));
-                $counter++;
-            }
+
+        $className = '\\app\\classes\\animals\\' . ucfirst($animal);
+
+        for ($i = 1; $i <= $quantity; $i++) {
+            $farm->addAnimal(new $className(uniqid($animal)));
         }
-        if ($animal === 'chicken') {
-            while ($counter <= $quantity) {
-                $farm->addAnimal(new Chicken(uniqid('chicken')));
-                $counter++;
-            }
-        }
+
     }
 }
 
-function weeklyProductsList(array $products)
+function showWeeklyProductsList(array $products)
 {
     foreach ($products as $productType => $productQuantity) {
         echo $productType . ' - ' . $productQuantity . "\n";
     }
 }
 
-function animalsList(array $animals)
+function showAnimalsList(array $animals)
 {
     foreach ($animals as $animalType => $animalQuantity) {
         echo $animalType . ' - ' . $animalQuantity . "\n";
     }
 }
 
-function weeklyProductCollection(Farm $farm)
+function getWeeklyProductCollection(Farm $farm)
 {
     for ($counter = 1; $counter <= 7; $counter++) {
         $farm->collectProduct();
